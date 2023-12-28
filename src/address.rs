1f849e9ab606cd7402f6a9c5753c8fde7cecc35f
Sam.zhi <sam@192.168.1.100>
2023-12-28 14:26:21 +0800

    init project

---
+++ src/address.rs
@@ -0,0 +1,17 @@
+
+fn mnemonic_address() -> String {
+
+    return "".to_string();
+}
+
+fn private2address() -> String {
+
+
+    "".to_string()
+}
+
+#[cfg(test)]
+pub mod tests {
+
+
+}
---
+++ src/bip39.rs
@@ -0,0 +1,8 @@
+
+
+
+#[cfg(test)]
+pub mod tests {
+
+
+}
